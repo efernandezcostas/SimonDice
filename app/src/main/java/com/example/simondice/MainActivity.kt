579d7a7ca26c0016fe0c6bd1ac985b19590c99ba
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-10-14 12:55:57 +0200

    a

--- app/src/main/java/com/example/simondice/MainActivity.kt
+++ app/src/main/java/com/example/simondice/MainActivity.kt
@@ -5,9 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -18,12 +20,20 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.simondice.ui.theme.SimonDiceTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,55 +52,89 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+enum class Colores { AZUL, ROJO, VERDE, AMARILLO }
+
+@Composable
+fun SimonDice() {
+
+}
+
 @Composable
 fun Botones() {
+    var listaColores = null
+    var puntuacion by remember { mutableIntStateOf(0) }
+
+    Row(
+        modifier = Modifier
+            .padding(top = 30.dp),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.Top,
+    ){
+        Text(
+            fontSize = 21.sp,
+            text = "Puntuación: $puntuacion",
+            modifier = Modifier
+                .padding(16.dp),
+            color = Color.Black
+        )
+    }
 
     Row(
         modifier = Modifier.fillMaxSize(),
         horizontalArrangement = Arrangement.SpaceEvenly,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Column {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
             Button(
                 onClick = {
-
+                    puntuacion++
                 },
                 modifier = Modifier
-                    .size(150.dp, 80.dp)
-                    .padding(16.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0,0,150))
+                    .aspectRatio(0.8f)
+                    .padding(10.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0,0,150)),
+                shape = RectangleShape,
             ) {
                 Text(
-                    text = "Azul"
+                    text = "Azul",
+                    fontSize = 20.sp
                 )
             }
             Button(
                 onClick = {
-
+                    puntuacion--
                 },
                 modifier = Modifier
-                    .size(150.dp, 80.dp)
-                    .padding(16.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(150,0,0))
+                    .aspectRatio(0.8f)
+                    .padding(10.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(150,0,0)),
+                shape = RectangleShape,
             ) {
                 Text(
-                    text = "Rojo"
+                    text = "Rojo",
+                    fontSize = 20.sp
                 )
             }
         }
 
-        Column {
+        Column(
+            modifier = Modifier.weight(1f)
+        ) {
             Button(
                 onClick = {
 
                 },
                 modifier = Modifier
-                    .size(150.dp, 80.dp)
-                    .padding(16.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(0,150,0))
+                    .aspectRatio(0.8f)
+                    .padding(10.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(0,150,0)),
+                shape = RectangleShape,
             ) {
                 Text(
-                    text = "Verde"
+                    text = "Verde",
+                    fontSize = 20.sp
                 )
             }
             Button(
@@ -98,14 +142,37 @@ fun Botones() {
 
                 },
                 modifier = Modifier
-                    .size(150.dp, 80.dp)
-                    .padding(16.dp),
-                colors = ButtonDefaults.buttonColors(containerColor = Color(204,204,0))
+                    .aspectRatio(0.8f)
+                    .padding(10.dp),
+                colors = ButtonDefaults.buttonColors(containerColor = Color(204,204,0)),
+                shape = RectangleShape,
             ) {
                 Text(
-                    text = "Amarillo"
+                    text = "Amarillo",
+                    fontSize = 20.sp
                 )
             }
+        }
+    }
+
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.Bottom
+    ){
+        Button(
+            onClick = { puntuacion = 0 },
+            modifier = Modifier
+                .padding(16.dp)
+                .aspectRatio(4f)
+                .border(1.dp, Color.Black),
+            colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.background),
+            shape = RectangleShape
+        ) {
+            Text(
+                text = "Empezar",
+                fontSize = 20.sp,
+                color = Color.Black
+            )
         }
     }
 }
