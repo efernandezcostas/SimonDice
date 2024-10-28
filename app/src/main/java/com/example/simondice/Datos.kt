43dc1077b3de35071009d457d6a6d383748ba434
efernandezcostas <efernandezcostas@danielcastelao.org>
2024-10-28 12:04:47 +0100

    comentados onClick y creadas funciones numero Random en ModelView

--- app/src/main/java/com/example/simondice/Datos.kt
+++ app/src/main/java/com/example/simondice/Datos.kt
@@ -1,4 +1,14 @@
 package com.example.simondice
 
-class Datos {
+import androidx.compose.ui.graphics.Color
+
+object Datos {
+    var numero = 0
+}
+
+enum class Colores (val color: Color, val nombre: String){
+    AZUL(Color(25, 25, 194, 255), "AZUL"),
+    ROJO(Color(199, 37, 37, 255), "ROJO"),
+    VERDE(Color(30, 178, 30, 255), "VERDE"),
+    AMARILLO(Color(222, 202, 18, 255), "AMARILLO")
 }
--- app/src/main/java/com/example/simondice/ModelView.kt
+++ app/src/main/java/com/example/simondice/ModelView.kt
@@ -1,4 +1,20 @@
 package com.example.simondice
 
+import android.util.Log
+
+
 class ModelView {
+
+    var numeroRandom = 0
+
+    fun crearRandom(){
+        numeroRandom = (1..4).random()
+        actualizarNumero()
+        Log.d("Numero", numeroRandom.toString())
+    }
+
+    fun actualizarNumero(){
+        Datos.numero = numeroRandom
+        Log.d("Numero", Datos.numero.toString())
+    }
 }
